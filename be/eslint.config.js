55a611283080620696b160e82cb666fce0e7409d
taraswww777 <taraswww777@mail.ru>
2025-01-04 14:31:37 +0300

    add eslint to be

---
+++ be/eslint.config.js
@@ -0,0 +1,17 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { files: ["./src/**/*.{js,mjs,cjs,ts}"] },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": 'warn'
+    }
+  }
+];
